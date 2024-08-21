3613a4952f47504eea72b60008682fea82fe661e
GAYATHRI V <139433748+Gaya3velmurugan@users.noreply.github.com>
2024-08-21 08:13:09 +0530

    Create Sum-of-Squares.js

---
+++ Sum-of-Squares.js
@@ -0,0 +1,15 @@
+function Gayathri(arr)
+{
+    let i;
+    let sum=0;
+    for(i=0;i<arr.length;i++)
+    { 
+        if(arr[i] = arr[i] ** 2) 
+        {
+            sum = sum + arr[i]; 
+        } 
+    }
+    return sum; 
+}
+let arr =[2,3,4,5]
+console.log("Sum of Squares of elements are: ", Gayathri(arr));
